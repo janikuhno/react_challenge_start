ea3b5cb62110a51cf05676a435d593059ac50707
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-11-18 15:58:11 +0200

    Contacts done

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,42 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Contact Name"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          pattern="358[4-5][0-9][0-9]{3}[0-9]{2}[0-9]{2}"
+          placeholder="Contact Phone"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Contact Email"
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Contact" />
+    </form>
   );
 };
